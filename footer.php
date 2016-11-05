708cf814fb4e3c6535c118e69b2ee07bddff383e
Carlos Longarela <carlos@longarela.eu>
2016-11-05 17:46:53 +0100

    Cambiadas tabulaciones a espacios

--- footer.php
+++ footer.php
@@ -11,15 +11,15 @@
 
 ?>
 
-	</div><!-- #content -->
+    </div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cl-marxia' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'cl-marxia' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'cl-marxia' ), 'cl-marxia', '<a href="https://desarrolloweb.longarela.eu/" rel="designer">Carlos Longarela</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+    <footer id="colophon" class="site-footer" role="contentinfo">
+        <div class="site-info">
+            <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cl-marxia' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'cl-marxia' ), 'WordPress' ); ?></a>
+            <span class="sep"> | </span>
+            <?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'cl-marxia' ), 'cl-marxia', '<a href="https://desarrolloweb.longarela.eu/" rel="designer">Carlos Longarela</a>' ); ?>
+        </div><!-- .site-info -->
+    </footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
